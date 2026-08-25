5977ae752fb2553454a31213544d1e5ded2a3021
softsbuild <maria.e.caetano8@aluno.senai.br>
2026-08-25 10:58:22 -0300

    vagas atualizado

--- js/vagas.js
+++ js/vagas.js
@@ -14,14 +14,56 @@ function desligarLampada() {
 
 function ocupada() {
 
-    const imagens = document.getElementById('img2')
+    const imagens = document.getElementById('img1')
     imagens.src = "/IMG/green-circle-emoji.jpg"
 
 }
 
 function desocupada() {
 
+    const imagens = document.getElementById('img1')
+    imagens.src = "/IMG/images.jpg";
+
+}
+
+function ligarLampada2() {
+
+    const imagens = document.getElementById('img2')
+    imagens.src = "/IMG/green-circle-emoji.jpg"
+
+}
+
+function desligarLampada2() {
+
     const imagens = document.querySelector('#img2')
+    imagens.src = "/IMG/images.jpg";
+
+}
+
+function ligarLampada3() {
+
+    const imagens = document.getElementById('img3')
+    imagens.src = "/IMG/green-circle-emoji.jpg"
+
+}
+
+function desligarLampada3() {
+
+    const imagens = document.querySelector('#img3')
+    imagens.src = "/IMG/images.jpg";
+
+}
+
+function ligarLampada4() {
+
+    const imagens = document.getElementById('img4')
+    imagens.src = "/IMG/green-circle-emoji.jpg"
+
+}
+
+function desligarLampada4() {
+
+    const imagens = document.querySelector('#img4')
     imagens.src = "/IMG/images.jpg";
 
 }
